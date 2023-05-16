c58a8706a571423f9e1b928610a5e2480df66d6a
Neil Wang <mostaron@126.com>
2023-05-16 14:13:09 +0800

    add 62~64, 1335

---
+++ 64/solution.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+
+}
+
+func minPathSum(grid [][]int) int {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if i == 0 && j == 0 {
+				continue
+			} else if i == 0 {
+				grid[i][j] += grid[i][j-1]
+			} else if j == 0 {
+				grid[i][j] += grid[i-1][j]
+			} else {
+				grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+			}
+
+		}
+	}
+	return grid[len(grid)-1][len(grid[0])-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
